0a31056dbaf40379229b47ec61401d97af8c16de
863153658@qq.com <863153658@qq.com>
2016-08-16 14:35:56 +0800

    延迟加载单例模式

--- src/DP1_4/Singleton/LazySingleton.java
+++ src/DP1_4/Singleton/LazySingleton.java
@@ -1,18 +1,26 @@
 package DP1_4.Singleton;
 
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
 /**
  * 	延迟加载单例模式
  * 	缺陷:多线程调用时可能不能保证单例性.
  * 		如:在多线程环境下,当线程1正新建单例时,完成赋值操作前,线程2可能判断instance为null,故线程2
  * 		也将启动新建单例的程序,而导致多个实例被创建.
  */
-public class LazySingleton
+public class LazySingleton implements Serializable
 {
 	private static LazySingleton instance= null;
 	
 	//私有构造方法
 	private LazySingleton()
 	{
+  		// 防止反射获取多个对象的漏洞  
+        if (null != instance) 
+        {  
+            throw new RuntimeException();  
+        } 
 	}
 	
 	//静态工厂方法
@@ -22,6 +30,12 @@ public class LazySingleton
 		{
 			instance = new LazySingleton();
 		}
+		return instance;
+	}
+	
+	//防止反序列化获取多个对象的漏洞
+	private Object readResolve() throws ObjectStreamException
+	{
 		return instance;
 	}
 }
