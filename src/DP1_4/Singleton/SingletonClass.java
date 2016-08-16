51acb8484e101c7fca0a8bac5fc5615863d73de5
863153658@qq.com <863153658@qq.com>
2016-08-16 14:09:04 +0800

    加同步代码块的单例模式

--- src/DP1_4/Singleton/SingletonClass.java
+++ src/DP1_4/Singleton/SingletonClass.java
@@ -1,8 +1,11 @@
 package DP1_4.Singleton;
 
+/**
+ *	加同步代码块的单例模式 
+ *
+ */
 public class SingletonClass
 {
-
 	private static SingletonClass instance = null;
 
 	public static SingletonClass getInstance()
